eb701bd022ce95f984287cc94320d161bf9124d1
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-02-28 10:50:33 +0500

    chore: inital project

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "A relay bot between telegram and matrix.";
+
+  inputs = {
+    # Stable for keeping thins clean
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    # Fresh and new for testing
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # The flake-parts library
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Pre commit hook
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
+        imports = [
+          inputs.git-hooks.flakeModule
+        ];
+        flake = {
+          # Overlay module
+          nixosModules.relay-bot = import ./module.nix self;
+        };
+        perSystem =
+          {
+            config,
+            pkgs,
+            ...
+          }:
+          {
+            # Pre-commit-hooks
+            pre-commit.settings.hooks = {
+              nixfmt.enable = true;
+              statix.enable = true;
+              clippy.enable = true;
+              rustfmt.enable = true;
+            };
+
+            # Development environment
+            devShells.default = import ./shell.nix self {
+              inherit pkgs config;
+            };
+
+            # Output package
+            packages.default = pkgs.callPackage ./. { inherit pkgs; };
+          };
+      }
+    );
+}
